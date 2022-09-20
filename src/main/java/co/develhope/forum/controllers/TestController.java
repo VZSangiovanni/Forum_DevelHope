2f17a3fd2fce13507c70b10f9f0984b6aceb8cb8
Alessio Grassia <alessiograssia2019@gmail.com>
2022-09-20 18:02:38 +0200

    added Delete User crud

--- src/main/java/co/develhope/forum/controllers/TestController.java
+++ src/main/java/co/develhope/forum/controllers/TestController.java
@@ -1,39 +1,80 @@
 package co.develhope.forum.controllers;
 
 
-
+import co.develhope.forum.dto.response.BaseResponse;
+import co.develhope.forum.dto.response.DeleteUserDTO;
+import co.develhope.forum.services.CustomUserService;
 import it.pasqualecavallo.studentsmaterial.authorization_framework.filter.AuthenticationContext;
 import it.pasqualecavallo.studentsmaterial.authorization_framework.security.PublicEndpoint;
 import it.pasqualecavallo.studentsmaterial.authorization_framework.security.RoleSecurity;
 import it.pasqualecavallo.studentsmaterial.authorization_framework.security.ZeroSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
+    @Autowired
+    CustomUserService customUserService;
 
-	@GetMapping("/default-deny")
-	public void defaultDenyEndpoint() {
-		System.out.println("Default deny. This endpoint cannot be reached");
-	}
 
-	@PublicEndpoint
-	@GetMapping("/public-endpoint")
-	public void publicEndpoint() {
-		System.out.println("This endpoint can be reached without authentication header");
-	}
+    @GetMapping("/default-deny")
+    public void defaultDenyEndpoint() {
+        System.out.println("Default deny. This endpoint cannot be reached");
+    }
 
-	@ZeroSecurity
-	@GetMapping("/no-role-endpoint")
-	public void noRoleEndpoint() {
-		System.out.println("This endpoint can be reached with any role, but require authentication. Authenticated user is " + AuthenticationContext.get().getUsername());
-	}
+    @PublicEndpoint
+    @GetMapping("/public-endpoint")
+    public void publicEndpoint() {
+        System.out.println("This endpoint can be reached without authentication header");
+    }
 
-	@RoleSecurity(value = { "ROLE_USER", "ROLE_ADMIN"})
-	@GetMapping("/role-evaluated-endpoint")
-	public void roleCheckEndpoint() {
-		System.out.println("This endpoint can be reached only by authenticated users with ROLE_USER or ROLE_ADMIN. Authenticated user is " + AuthenticationContext.get().getUsername());
-	}
+    @ZeroSecurity
+    @GetMapping("/no-role-endpoint")
+    public void noRoleEndpoint() {
+        System.out.println("This endpoint can be reached with any role, but require authentication. Authenticated user is " + AuthenticationContext.get().getUsername());
+    }
+
+    @RoleSecurity(value = {"ROLE_USER", "ROLE_ADMIN"})
+    @GetMapping("/role-evaluated-endpoint")
+    public void roleCheckEndpoint() {
+        System.out.println("This endpoint can be reached only by authenticated users with ROLE_USER or ROLE_ADMIN. Authenticated user is " + AuthenticationContext.get().getUsername());
+    }
+
+
+    @ZeroSecurity
+    @DeleteMapping("delete-self")
+    public BaseResponse deleteSelf() {
+
+        AuthenticationContext.Principal principal = AuthenticationContext.get();
+
+        if (customUserService.deleteUser(principal.getUsername())) {
+
+            return new BaseResponse();
+
+        } else {
+
+            return new BaseResponse("Delete failed");
+        }
+
+
+    }
+
+
+    @RoleSecurity(value = {"ROLE_FOUNDER"})
+    @DeleteMapping("delete-user")
+    public BaseResponse deleteUser(@RequestBody DeleteUserDTO deleteUserDTO) {
+        if (customUserService.deleteUser(deleteUserDTO.getUsername())) {
+
+            return new BaseResponse();
+
+        } else {
+
+            return new BaseResponse("Delete failed");
+        }
+
+    }
+
 
 }
