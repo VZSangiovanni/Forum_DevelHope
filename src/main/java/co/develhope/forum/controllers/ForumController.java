c766ca19355e0406ddfd61ee77d8cb3091be9ea1
omaima96 <omaimam1396@gmail.com>
2022-10-02 16:42:25 +0200

    feature-topic-crud-read

--- src/main/java/co/develhope/forum/controllers/ForumController.java
+++ src/main/java/co/develhope/forum/controllers/ForumController.java
@@ -100,7 +100,7 @@ public class ForumController {
     }
 
     @ZeroSecurity
-    @GetMapping("/read-post-by-gittopic/{id}")
+    @GetMapping("/read-post-by-topic/{id}")
     public List<Map<String, Object>> readPostsByTopic(@PathVariable int id) {
         return forumService.findAllPostsByTopic(id);
     }
